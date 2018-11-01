322e4da62bfc7d39f83593f841c65723efdb663d
Rafa <rafa@pop-os.localdomain>
2018-11-01 16:43:15 -0700

    Security

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+    http_basic_authenticate_with name: "xipe", password: "123456", except: [:index, :show]
+
     def index
         @articles = Article.all
       end
