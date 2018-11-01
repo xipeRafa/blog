4733ff7ab7be145a08f08efbeeb56301b20b1b33
Rafa <rafa@pop-os.localdomain>
2018-11-01 16:39:49 -0700

    Deleting Comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ApplicationRecord
-    has_many :comments
+    has_many :comments, dependent: :destroy
     validates :title, presence: true,
     length: { minimum: 5 }
 end
+
+
+#If you delete an article, its associated comments will also need to be deleted, otherwise they would simply occupy #space in the database. Rails allows you to use the dependent option of an association to achieve this. Modify the #Article model, app/models/article.rb, as follows: --->  has_many :comments, dependent: :destroy
